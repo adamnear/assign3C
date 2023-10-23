c3a8ab0b43c619cbde3a96ab808ed15665586a23
adamnear <nearing14@hotmail.com>
2023-10-23 17:51:02 -0300

    Fixing the geojson collection of features

--- PROG2700-Assignment3-StarterCode-Flight/main.js
+++ PROG2700-Assignment3-StarterCode-Flight/main.js
@@ -16,25 +16,37 @@
                 //Use filter method to only use data from Canadian flights
                 const canadianFlights = data.states.filter(flight => flight[2] = 'Canada');
 
-                //Create a GeoJSON Object
-                const geoJsonFeatures = canadianFlights.map(flight => ({
-                    type: 'Feature',
-                    geometry: {
-                        type: 'Point', //Specifying the flight is a point on the map
-                        coordinates: [flight[5], flight[6]] // Latitude and longitude
-                    },
-                    properties: {
-                        callsign: flight[1],
-                        origin: flight[2],
-                        altitude: flight[7]
-                    }
-                }));
+                // Create a GeoJSON FeatureCollection
+                const geoJsonFeatureCollection = {
+                    type: 'FeatureCollection',
+                    features: canadianFlights.map(flight => ({
+                        type: 'Feature',
+                        geometry: {
+                            type: 'Point', //Specifying the flight is a point on the map
+                            coordinates: [flight[5], flight[6]] // Latitude and longitude
+                        },
+                        properties: {
+                            callsign: flight[1],
+                            origin: flight[2],
+                            altitude: flight[7]
+                        }
+                    }))
+                };
 
                 //Add markers to the map
-                L.geoJSON({
+                L.geoJSON(geoJsonFeatureCollection, {
                     type: 'FeatureCollection',
                     features: geoJsonFeatures
                 }).addTo(map);
             })
+            .catch(error => {
+                console.error('Error fetching flight data:', error);
+            });
     }
+    // Fetch and display flight data initially
+    fetchFlightData();
+
+    // Set up an interval to auto-refresh the flight data every 5 minutes (300,000 milliseconds)
+    setInterval(fetchFlightData, 300000);
+
 })()
