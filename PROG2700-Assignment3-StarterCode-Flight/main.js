6c673afe19f2927a4f9db4c27774a7fd87b5a353
adamnear <nearing14@hotmail.com>
2023-10-23 15:48:39 -0300

    Center over toronto, zoom in

--- PROG2700-Assignment3-StarterCode-Flight/main.js
+++ PROG2700-Assignment3-StarterCode-Flight/main.js
@@ -2,15 +2,13 @@
 (() => {
 
     //create map in leaflet and tie it to the div called 'theMap'
-    let map = L.map('theMap').setView([42, -60], 4);
+    let map = L.map('theMap').setView([43.6532, -79.3832], 8);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
     L.marker([42, -60]).addTo(map)
         .bindPopup('This is a sample popup. You can put any html structure in this including extra flight data. You can also swap this icon out for a custom icon. Some png files have been provided for you to use if you wish.')
-        .openPopup();
-
 
 })()
