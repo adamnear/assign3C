216cbcedced0b49dff540fb0e30846660e2c6dc3
adamnear <nearing14@hotmail.com>
2023-10-23 16:14:08 -0300

    fetching flight data from Opensky Network API

--- PROG2700-Assignment3-StarterCode-Flight/main.js
+++ PROG2700-Assignment3-StarterCode-Flight/main.js
@@ -8,7 +8,12 @@
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker([42, -60]).addTo(map)
-        .bindPopup('This is a sample popup. You can put any html structure in this including extra flight data. You can also swap this icon out for a custom icon. Some png files have been provided for you to use if you wish.')
+    //function to fetch the flight data from OpenSky Network API
+    function fetchFlightData() {
+        fetch('https://opensky-network.org/api/states/all')
+            .then(response => response.json())
+            .then(data => {
 
+            })
+    }
 })()
